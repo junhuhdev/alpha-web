90e52e5d7399c2c7db5ff82b553715395cb8890c
Juan Young Huh <juan.huh@evry.com>
2019-07-30 22:57:57 +0200

    Update NavbarHeader.vue

--- src/components/common/NavbarHeader.vue
+++ src/components/common/NavbarHeader.vue
@@ -1,11 +1,15 @@
 <template>
-  <v-app-bar app color="deep-purple accent-4" dark>
+  <v-app-bar app dense color="deep-purple accent-4" dark>
     <v-app-bar-nav-icon></v-app-bar-nav-icon>
-    <v-toolbar-title>
-      <router-link to="/" class="toolbar-title">Heartbeat</router-link>
-    </v-toolbar-title>
-    <v-btn text v-for="item in items" :key="item.title" :to="item.link">{{item.title}}</v-btn>
-    <v-spacer></v-spacer>
+    <div class="mr-5">
+      <v-toolbar-title>
+        <router-link to="/" class="toolbar-title">Heartbeat</router-link>
+      </v-toolbar-title>
+    </div>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn text v-for="item in items" :key="item.title" :to="item.link">{{item.title}}</v-btn>
+    </v-toolbar-items>
+    <!--    <v-btn text v-for="item in items" :key="item.title" :to="item.link">{{item.title}}</v-btn>-->
   </v-app-bar>
 </template>
 <script>
@@ -16,9 +20,9 @@
    items: [
     {title: 'Login', link: '/login', icon: 'home'},
     {title: 'Register', link: '/register', icon: 'home'},
+    {title: 'User', link: '/user', icon: 'home'},
     {title: 'Dashboard', link: '/dashboard', icon: 'home'},
     {title: 'Diary', link: '/diary', icon: 'home'},
-    {title: 'User', link: '/user', icon: 'home'},
    ]
   })
 
