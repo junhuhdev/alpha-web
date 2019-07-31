8e739b766e197a4f1cec6a418f4d33daec70185a
Juan Young Huh <juan.huh@evry.com>
2019-07-31 02:23:58 +0200

    x

--- src/views/directory/CreateTobacco.vue
+++ src/views/directory/CreateTobacco.vue
@@ -1,5 +1,22 @@
 <template>
-
+  <v-layout align-center justify-center>
+    <v-flex xs12 sm8 md4>
+      <v-card class="elevation-12">
+        <v-toolbar color="primary" dark flat>
+          <v-toolbar-title>Create tobacco</v-toolbar-title>
+        </v-toolbar>
+        <v-card-text>
+          <v-form>
+            <v-text-field label="Name" name="name" type="text"></v-text-field>
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary">Create</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 <script>
  export default {
