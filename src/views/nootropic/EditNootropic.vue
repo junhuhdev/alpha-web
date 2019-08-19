6e8ae883d8a41b7f52a642643794956ee4fd25fd
Juan Young Huh <juan.huh@evry.com>
2019-08-19 22:04:08 +0200

    x

--- src/views/nootropic/EditNootropic.vue
+++ src/views/nootropic/EditNootropic.vue
@@ -1,9 +1,48 @@
 <template>
-
+  <v-layout align-center justify-center>
+    <v-flex xs12 sm8 md12>
+      <v-card class="elevation-12">
+        <v-toolbar color="deep-purple accent-4" dark flat>
+          <v-toolbar-title>{{title}}</v-toolbar-title>
+        </v-toolbar>
+        <v-card-text>
+          <v-form>
+            <v-text-field v-model="nootropic.name" label="Name" name="name" type="text"></v-text-field>
+            <v-text-field v-model="nootropic.manufacturer" label="Manufacturer" name="manufacturer" type="text"></v-text-field>
+            <v-text-field v-model="nootropic.link" label="Link" name="link" type="text"></v-text-field>
+            <v-textarea v-model="nootropic.ingredients" label="Ingredients" auto-grow rows="2" row-height="15"></v-textarea>
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="warning" @click="closeParent">Cancel</v-btn>
+          <v-btn v-if="createMode" color="primary" @click="insert">Create</v-btn>
+          <v-btn v-if="!createMode" color="primary" @click="update">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 <script>
  export default {
-  name: 'EditNootropic'
+  props: {
+   title: String,
+   createMode: Boolean,
+   nootropic: Object,
+   closeParent: Function
+  },
+
+  methods: {
+   insert () {
+    this.$store.dispatch('insertNootropic', this.nootropic)
+     .then(() => this.closeParent());
+   },
+
+   update () {
+    this.$store.dispatch('updateNootropic', this.nootropic)
+     .then(() => this.closeParent());
+   }
+  }
  };
 </script>
 <style scoped>
--- src/views/nootropic/Nootropic.vue
+++ src/views/nootropic/Nootropic.vue
@@ -12,32 +12,97 @@
               :headers="headers"
               :items="nootropics"
               :search="search"
-          ></v-data-table>
+          >
+            <template v-slot:top>
+              <v-toolbar flat color="white">
+                <v-dialog v-model="dialog" max-width="800px">
+                  <template v-slot:activator="{on}">
+                    <v-btn v-on="on" absolute small dark fab right color="grey">
+                      <v-icon>mdi-plus</v-icon>
+                    </v-btn>
+                  </template>
+                  <EditNootropic
+                      v-bind:nootropic="editedItem"
+                      v-bind:title="formTitle"
+                      v-bind:createMode="createMode"
+                      v-bind:closeParent="close"
+                  ></EditNootropic>
+                </v-dialog>
+              </v-toolbar>
+            </template>
+            <template v-slot:item.action="{item}">
+              <v-icon small class="mr-2" @click="editItem(item)">mdi-pencil</v-icon>
+              <v-icon small class="mr-2" @click="deleteItem(item)">mdi-delete</v-icon>
+            </template>
+          </v-data-table>
         </v-card-text>
       </v-card>
     </v-flex>
   </v-layout>
 </template>
 <script>
+ import { mapGetters } from 'vuex';
+ import EditNootropic from './EditNootropic';
+
  export default {
+  components: {EditNootropic},
   data: () => ({
+   dialog: false,
    search: '',
    headers: [
     {text: 'Name', value: 'name'},
     {text: 'Manufacturer', value: 'manufacturer'},
     {text: 'Ingredients', value: 'ingredients'},
     {text: 'Link', value: 'link'},
-   ]
+    {text: '', value: 'action', align: 'end'},
+   ],
+   editedIndex: -1,
+   editedItem: {},
+   defaultItem: {}
   }),
 
+  watch: {
+   dialog (val) {
+    val || this.close();
+   }
+  },
+
   computed: {
-   nootropics () {
-    return this.$store.getters.nootropics;
+   ...mapGetters([
+    'nootropics'
+   ]),
+
+   formTitle () {
+    return this.editedIndex === -1 ? 'Create nootropic' : 'Edit nootropic';
+   },
+
+   createMode () {
+    return this.editedIndex === -1;
    }
   },
 
   created () {
    this.$store.dispatch('selectNootropics');
+  },
+
+  methods: {
+   editItem (item) {
+    this.editedIndex = this.nootropics.indexOf(item);
+    this.editedItem = Object.assign({}, item);
+    this.dialog = true;
+   },
+
+   deleteItem (item) {
+    confirm('Are you sure you want to delete?') && this.$store.dispatch('deleteNootropic', item);
+   },
+
+   close () {
+    this.dialog = false;
+    setTimeout(() => {
+     this.editedItem = Object.assign({}, this.defaultItem);
+     this.editedIndex = -1;
+    }, 300);
+   }
   }
  };
 </script>
